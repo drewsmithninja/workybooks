e9369c50a2e5d6d7b555e4fb34e4d8a2bf9959fe
Maxwell Smith <maxwell@ninjatechnolabs.com>
2022-09-29 12:42:24 +0530

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,39 +26,44 @@ function Home() {
     <MainLayout>
       {user.loggedIn && (
         <div className='w-full max-w-[95%] m-auto'>
-          <div className='!mt-[20px] border-1 border-solid border-x-0 border-t-0 pb-2'>
+          <div className='text-center sm:text-left !mt-[20px] sm:border-1 sm:border-solid border-x-0 border-t-0 pb-2'>
             <Row gutter={[16, 16]}>
-              <Col span={1} />
-              <Col span={14} className='border-1 border-solid border-l-0 border-y-0'>
-                <Typography.Text className='font-bold text-[11px] ml-[40px] !mb-[20px]'>Browse by Subject</Typography.Text>
+              <Col md={1} sm={0} />
+              <Col md={11} sm={24} className='sm:border-1 sm:border-solid border-l-0 border-y-0'>
+                <Typography.Text className='font-bold text-[11px] sm:ml-[40px] sm:mb-0 !mb-[20px] block'>Browse by Subject</Typography.Text>
                 <Row gutter={[16, 16]}>
                   {Array(6).fill(1).map(() => (
-                    <Col span={4} key={`subject_${Math.random()}`}>
+                    <Col md={4} xs={8} key={`subject_${Math.random()}`}>
                       <SubjectComponent subjectImage={dummyVector} subjectName='BA' />
                     </Col>
                   ))}
                 </Row>
               </Col>
               <Col span={0} />
-              <Col span={4}>
-                <Typography.Text className='font-bold text-[11px] ml-[10px]'>Browse by Common core standards</Typography.Text>
+              <Col md={5} sm={24} xs={24} className='!p-0 text-center sm:text-left'>
+                <Typography.Text className='font-bold text-[11px] md:ml-[10px] w-full  mb-[20px] block'>Browse by Common core standards</Typography.Text>
                 <Row gutter={[16, 16]}>
                   {Array(2).fill(1).map(() => (
-                    <Col span={12} key={`subject_${Math.random()}`}>
+                    <Col md={12} sm={12} xs={12} key={`subject_${Math.random()}`}>
                       <SubjectComponent subjectImage={dummyVector} subjectName='BA' />
                     </Col>
                   ))}
                 </Row>
               </Col>
-              <Col span={2} />
+              <Col md={2} sm={0} />
             </Row>
           </div>
 
           <div className='flex items-center justify-center pt-[20px]'>
-            <span className='font-normal text-sm pr-[20px]'>Grade</span>
-            <Radio.Group defaultValue='1' size='small' buttonStyle='solid'>
+            <span className='font-normal text-sm sm:pr-[20px]'>Grade</span>
+            <Radio.Group
+              defaultValue='1'
+              size='small'
+              buttonStyle='solid'
+              className='h-[60px] sm:h-auto flex flex-wrap sm:!max-w-full max-w-[320px] items-center justify-center'
+            >
               {grades.map((item, index) => (
-                <Radio.Button value={`${item}`} key={`${item}`} className='mr-[10px] w-[58px] h-[25px] bg-[#D9D9D9] !rounded-[60px] text-xs text-center border-0'>{item}</Radio.Button>
+                <Radio.Button value={`${item}`} key={`${item}`} className='mr-[10px] w-[58px] h-[25px] bg-[#D9D9D9] !rounded-[60px] text-xs text-center border-0 mb-[0px] sm:mb-0'>{item}</Radio.Button>
               ))}
             </Radio.Group>
           </div>
