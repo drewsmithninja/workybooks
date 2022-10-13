68fa8c6177666671f097d3ec681bd47cf062a4be
Daniel Smith <daniel@ninjatechnolabs.com>
2022-10-13 16:15:59 +0530

    worksheet page ui done with responsive

--- src/pages/worksheet/Worksheet.jsx
+++ src/pages/worksheet/Worksheet.jsx
@@ -1,5 +1,8 @@
-import { Col, Row, Space, Tag } from 'antd';
+import { FaPrint, FaFolderPlus, FaRegImages, FaPinterest, FaFacebook, FaInstagram, FaLink } from 'react-icons/fa';
+import { MdAssignmentTurnedIn } from 'react-icons/md';
+import { Col, Image, Row, Space, Tag } from 'antd';
 import React from 'react';
+import dummyImage from '../../assets/images/dummyImage.png';
 import ADButton from '../../components/antd/ADButton';
 import ADTitle from '../../components/antd/ADTitle';
 import MainLayout from '../../components/layout/MainLayout';
@@ -7,22 +10,56 @@ import MainLayout from '../../components/layout/MainLayout';
 function Worksheet() {
   return (
     <MainLayout>
-      <div className='container pt-16'>
-        <Row gutter={24}>
-          <Col xs={24} md={6}>
-            <Space direction='vertical' className='w-full'>
-              <div className='bg-gray-300 h-[400px] rounded w-full' />
-              <ADButton type='primary' className='w-full'>
-                PLAY WORKSHEET
-              </ADButton>
-            </Space>
-            <div className='grid grid-cols-3 py-4'>
-              <div className='text-center bg-gray-100'>01</div>
-              <div className='text-center bg-gray-000'>02</div>
-              <div className='text-center bg-gray-100'>03</div>
+      <div className='container pt-16 md:px-0 px-4'>
+        <Row gutter={[16, 16]} className='pb-8'>
+          <Col xs={24} md={12} lg={8} xl={6}>
+            <div className='xl:pr-10 lg:pr-6 md:pr-4 pr-0'>
+              <Space direction='vertical' className='w-full'>
+                <div className='bg-gray-300 aspect-[2.5/3] w-full rounded w-full' />
+                <ADButton type='primary' className='w-full'>
+                  PLAY WORKSHEET
+                </ADButton>
+              </Space>
+              <Row gutter={16} className='text-gray-400 pt-6 pb-5'>
+                <Col lg={8} className='flex items-center border-y-0 border-l-0 border-solid'>
+                  <div className='text-2xl mr-2 flex'>
+                    <FaPrint />
+                  </div>
+                  <div className='text-xs text-gray-500'>PRINT</div>
+                </Col>
+                <Col lg={8} className='flex items-center border-y-0 border-l-0 border-solid'>
+                  <div className='text-2xl mr-2 flex'>
+                    <MdAssignmentTurnedIn />
+                  </div>
+                  <div className='text-xs text-gray-500'>ASSIGN</div>
+                </Col>
+                <Col lg={8} className='flex items-center'>
+                  <div className='text-2xl mr-2 flex'>
+                    <FaFolderPlus />
+                  </div>
+                  <div className='text-xs text-gray-500 leading-snug'>ADD TO COLLECTION</div>
+                </Col>
+              </Row>
+              <div className='border border-solid border-slate-300 rounded-2xl py-4 px-3 text-slate-400'>
+                <div className='flex items-center justify-between'>
+                  <div className='flex mx-0'>SHARE</div>
+                  <div className='text-2xl flex  mx-2'>
+                    <FaRegImages />
+                  </div>
+                  <div className='text-2xl flex  mx-2'>
+                    <FaPinterest />
+                  </div>
+                  <div className='text-2xl flex  mx-2'>
+                    <FaInstagram />
+                  </div>
+                  <div className='text-2xl flex  mx-2'>
+                    <FaLink />
+                  </div>
+                </div>
+              </div>
             </div>
           </Col>
-          <Col xs={24} md={18}>
+          <Col xs={24} md={12} lg={16} xl={18}>
             <ADTitle level={2}>short passage - find the meaning of a word - Ruth Bader</ADTitle>
             <div className='grid grid-cols-2 py-4 w-fit mb-4'>
               <div>Publisher:</div>
@@ -68,6 +105,14 @@ function Worksheet() {
             </div>
           </Col>
         </Row>
+        <ADTitle level={4}>Similar worksheets</ADTitle>
+        <Space size='large' className='overflow-x-auto w-full py-6'>
+          {Array(15)
+            .fill()
+            .map((i) => (
+              <Image width={200} key={i} src={dummyImage} />
+            ))}
+        </Space>
       </div>
     </MainLayout>
   );
