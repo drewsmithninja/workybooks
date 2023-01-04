289d278afdd594a94aa750e6732ef0a5750f9a9b
drewsmithninja <96779205+drewsmithninja@users.noreply.github.com>
2023-01-04 11:02:22 +0530

    Revert "fixing some esLint issues"

--- src/app/api/searchApi.js
+++ src/app/api/searchApi.js
@@ -6,50 +6,36 @@ const API_URL = process.env.REACT_APP_API_URL;
 const search = async (searchText) => {
   const user = localStorage.getItem('user');
   const authToken = JSON.parse(user)?.payload?.verification?.token;
-  const response = await axios.post(
-    `${API_URL}/content/getBy/subGradeCcsTopic/search`,
-    searchText,
-    {
-      headers: {
-        authorization: authToken
-      }
+  const response = await axios.post(`${API_URL}/content/getBy/subGradeCcsTopic/search`, searchText, {
+    headers: {
+      authorization: authToken
     }
-  );
+  });
   return response.data;
 };
 
 const searchSuggest = async (searchText) => {
   const user = localStorage.getItem('user');
   const authToken = JSON.parse(user)?.payload?.verification?.token;
-  const response = await axios.post(
-    `${API_URL}/content/getBy/keyw/ccs/sub/suggestion/search`,
-    searchText,
-    {
-      headers: {
-        authorization: authToken
-      }
+  const response = await axios.post(`${API_URL}/content/getBy/keyw/ccs/sub/suggestion/search`, searchText, {
+    headers: {
+      authorization: authToken
     }
-  );
+  });
   return response.data;
 };
 
 const subjectTopic = async (searchText) => {
   const user = localStorage.getItem('user');
   const authToken = JSON.parse(user)?.payload?.verification?.token;
-  const response = await axios.post(
-    `${API_URL}/subject/getBy/subject/topic/content/list`,
-    searchText
-  );
+  const response = await axios.post(`${API_URL}/subject/getBy/subject/topic/content/list`, searchText);
   return response.data;
 };
 
 const ccsTopic = async (searchText) => {
   const user = localStorage.getItem('user');
   const authToken = JSON.parse(user)?.payload?.verification?.token;
-  const response = await axios.post(
-    `${API_URL}/commonCoreStandard/getBy/ccs/topic/content/list`,
-    searchText
-  );
+  const response = await axios.post(`${API_URL}/commonCoreStandard/getBy/ccs/topic/content/list`, searchText);
   return response.data;
 };
 
