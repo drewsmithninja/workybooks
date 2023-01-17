3ad5e7e4da7dfd62db929b8ea114c86167491227
drewsmithninja <114493992+danielSmithFullStack@users.noreply.github.com>
2023-01-17 18:01:07 +0530

    add feature: create/update assign base on collection in details page

--- src/components/modals/AssignCollectionModal.jsx
+++ src/components/modals/AssignCollectionModal.jsx
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAssignments, setCurrentStep } from '../../app/features/assignment/assignmentSlice';
 import { resetSelectedCollections } from '../../app/features/collection/collectionSlice';
-import { resetSelectedWorksheets } from '../../app/features/worksheet/worksheetSlice';
 import ADModal from '../antd/ADModal';
 import ADSteps from '../antd/ADSteps';
 import AssignStep2 from '../steps/assign/AssignStep2';
@@ -10,13 +9,16 @@ import AssignStep3 from '../steps/assign/AssignStep3';
 import NewAssignment from '../steps/assign/NewAssignment';
 import AssignCollectionStep1 from '../steps/AssignCollectionStep1';
 
-function AssignCollectionModal({ onOk, ...props }) {
+function AssignCollectionModal({ onOk, inDetail, ...props }) {
   const currentStep = useSelector((state) => state.assignment.currentStep);
-  const [current, setCurrent] = useState(0);
   const dispatch = useDispatch();
 
   const next = () => {
-    dispatch(setCurrentStep(currentStep + 1));
+    if (inDetail) {
+      dispatch(setCurrentStep(currentStep + 2));
+    } else {
+      dispatch(setCurrentStep(currentStep + 1));
+    }
   };
 
   const afterClose = () => {
@@ -26,7 +28,15 @@ function AssignCollectionModal({ onOk, ...props }) {
   const onClose = () => {
     dispatch(setCurrentStep(0));
     dispatch(resetSelectedCollections());
-    onCancel();
+    onOk();
+  };
+
+  const onChangeHandler = (e) => {
+    if (inDetail) {
+      if (e !== 1) {
+        dispatch(setCurrentStep(e));
+      }
+    }
   };
 
   const items = [
@@ -37,23 +47,23 @@ function AssignCollectionModal({ onOk, ...props }) {
     {
       title: 'Select Items',
       icon: <>1</>,
-      content: <AssignCollectionStep1 next={next} onClose={onClose} onOk={onOk} {...props} />
+      content: <AssignCollectionStep1 inDetail={inDetail} next={next} onClose={onClose} onOk={onOk} {...props} />
     },
     {
       title: 'Select Students',
       icon: <>2</>,
-      content: <AssignStep2 next={next} onClose={onClose} onOk={onOk} {...props} />
+      content: <AssignStep2 inDetail={inDetail} next={next} onClose={onClose} onOk={onOk} {...props} />
     },
     {
       title: 'Set Assignment Details',
       icon: <>3</>,
-      content: <AssignStep3 onOk={onOk} onClose={onClose} {...props} />
+      content: <AssignStep3 inDetail={inDetail} onOk={onOk} onClose={onClose} {...props} />
     }
   ];
 
   return (
     <ADModal centered afterClose={afterClose} closable={false} footer={null} width={680} {...props}>
-      <ADSteps items={items} current={currentStep} onChange={(e) => dispatch(setCurrentStep(e))} showSteps={currentStep !== 0} className='custom-assign-steps' />
+      <ADSteps items={items} current={currentStep} onChange={(e) => onChangeHandler(e)} showSteps={currentStep !== 0} className='custom-assign-steps' />
       <div className='flex flex-col items-center justify-center'>
         <div className='steps-content max-w-[600px]'>{items[currentStep]?.content}</div>
       </div>
@@ -62,3 +72,7 @@ function AssignCollectionModal({ onOk, ...props }) {
 }
 
 export default AssignCollectionModal;
+
+AssignCollectionModal.defaultProps = {
+  inDetail: false
+};
